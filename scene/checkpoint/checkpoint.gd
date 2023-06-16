33b315d9a1b84f05a87cec286630276ef3e1e151
wiratana <andikawiratana@gmail.com>
2023-06-17 00:40:23 +0700

    create basic checkpoint event

---
+++ scene/checkpoint/checkpoint.gd
@@ -0,0 +1,38 @@
+class_name Checkpoint extends Node2D
+
+signal get_finish
+
+@onready var animation_player = $AnimationPlayer
+@onready var start_point	  = $start_point
+@onready var end_point		  = $end_point
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if self.visible:
+		animation_player.play("aura_idle")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered():
+	self.visible = true
+	animation_player.play("aura_idle")
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	self.visible = false
+	animation_player.stop()
+
+
+func _on_start_point_area_body_exited(body):
+	if body.is_in_group("player"):
+		start_point.queue_free()
+		
+
+
+func _on_end_point_area_body_entered(body):
+	if body.is_in_group("player"):
+		emit_signal("get_finish")
