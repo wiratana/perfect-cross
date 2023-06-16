a787e4a0c59a9af0ee615d04ac699c63de9a848c
wiratana <andikawiratana@gmail.com>
2023-06-17 01:00:59 +0700

    fix bug : player-system

--- scene/map.gd
+++ scene/map.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+@onready var map = $TileMap
+@onready var player_system = $player_system
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player_system.player.map = map
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
