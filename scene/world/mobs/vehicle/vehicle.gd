96df218f320e98ed7c97670ce380099c15d6c707
wiratana <andikawiratana@gmail.com>
2023-07-13 16:28:57 +0700

    vehicle car module update : animation, logic, integration

--- scene/world/mobs/vehicle/vehicle.gd
+++ scene/world/mobs/vehicle/vehicle.gd
@@ -1,35 +1,49 @@
 class_name Vehicle extends Mob
 
+@export  var direction:Route.DIRECTION = Route.DIRECTION.LEFT
+@onready var animationPlayer = $AnimationPlayer
+@onready var do_process = false
 var initial_crossing_area_entered
 var final_crossing_area_entered
 var entity_detected = false
 var must_go = false
 
+func animation():
+	match direction:
+		Route.DIRECTION.LEFT:
+			animationPlayer.play("left")
+		Route.DIRECTION.RIGHT:
+			animationPlayer.play("right")
+		Route.DIRECTION.UP:
+			animationPlayer.play("up")
+		Route.DIRECTION.BUTTOM:
+			animationPlayer.play("buttom")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	self.get_node("Sprite2D").hide()
+	animation()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
+	if(do_process):
+		pass
 	pass
 
 func must_stop():
-	if self.entity_detected:
-		return true
-	
 	if self.must_go:
 		return false #negative stop
-		
-	if self.initial_crossing_area_entered != null:
+	elif self.entity_detected:
+		return true
+	elif self.initial_crossing_area_entered != null:
 		return self.initial_crossing_area_entered.vehicle_stop_status as bool
-		
 	return false
 
 func _on_entity_detector_body_entered(body):
-	if(body.is_in_group("player") || body.is_in_group("padestrian")):
+	if(body.is_in_group("player") || body.is_in_group("padestrian") || body.is_in_group("vehicle")):
 		self.entity_detected = true
 
-func _on_entity_detector_body_exited(body):
+func _on_entity_detector_body_exited(_body):
 	self.entity_detected = false
 
 func _on_end_of_vision_area_entered(area):
@@ -48,3 +62,15 @@ func _on_center_position_area_exited(area):
 		must_go = false
 		final_crossing_area_entered = null
 		
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered():
+	self.get_node("Sprite2D").show()
+	self.do_process = true
+	SoundPlayer.play_sfx(SoundPlayer.SOUND.VEHICLE)
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	self.get_node("Sprite2D").hide()
+	self.do_process = false
+	SoundPlayer.stop_sfx(SoundPlayer.SOUND.VEHICLE)
